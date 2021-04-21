0346ed7a5b236cca0e54e4394d070a9b7763cef7
simply-joe <joekrav19@gmail.com>
2021-04-20 21:57:10 -0400

    rss metadata update

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Joe Krawiec',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    site_url: 'https://joekrawiec.com',
+    feed_url: 'https://joekrawiec.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))

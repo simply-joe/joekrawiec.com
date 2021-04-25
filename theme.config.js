52a8acf94acc51ff17a2cd79c16867e960ebd7e8
simply-joe <joekrav19@gmail.com>
2021-04-25 15:09:36 -0400

    update footer

--- theme.config.js
+++ theme.config.js
@@ -3,6 +3,13 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '4rem' }}>
+      <div>
+        GitHub: [@simply-joe](https://github.com/simply-joe)
+      <br />
+        Linkedin: [@joseph-krawiec](https://www.linkedin.com/in/joseph-krawiec/)
+      <br />
+        Email: joe [at] joekrawiec [dot] com
+      </div>
       <time>{YEAR}</time> © Joe Krawiec.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
